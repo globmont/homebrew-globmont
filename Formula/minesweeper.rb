430d99d2e62dbb3939271638ea5a2c2c018d0fd1
Nikhil Prasad <nikhil@globmont.local>
2017-03-02 22:28:09 -0500

    added first version of minesweeper

---
+++ Formula/minesweeper.rb
@@ -0,0 +1,37 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Minesweeper < Formula
+  desc "An implementation of minesweeper that runs in the terminal"
+  homepage "http://nikhilprasad.com"
+  url "https://github.com/globmont/minesweeper/archive/v1.0.tar.gz"
+  sha256 "88ab794bce6b92c1e79caeff732c234bc78b744b067d1b97b84d977bf428aca1"
+
+  depends_on :python3
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    # system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test minesweeper`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
